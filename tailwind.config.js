f18734beac38e0ee75a895d5a8a868ffd43b6225
Shivansh Chopra <gauravshivansh7@gmail.com>
2024-06-09 15:17:07 +0530

    uploading

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Adjust the path as necessary
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'clash-display': ['Clash Display', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
